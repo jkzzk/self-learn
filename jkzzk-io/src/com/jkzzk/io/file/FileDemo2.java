fccebe988aa97be879dbf11705e0eec29a3cf413
jkzzk <1745493589@qq.com>
2019-11-25 11:48:10 +0800

    提交工程文件

---
+++ jkzzk-io/src/com/jkzzk/io/file/FileDemo2.java
@@ -0,0 +1,23 @@
+package com.jkzzk.io.file;
+
+/**
+ *  路径：
+ *      绝对路径：是一个完整的路径
+ *          以盘符开始（C:、D:）的路径
+ *          C:\\a.txt
+ *          G:\\LearnRecord\\JAVA\\1_Program\\8_self-learn\\jkzzk-io\\jkzzk-io.iml
+ *          D:\\jkzzk.txt
+ *      相对路径：是一个简化的路径
+ *          相对是指的相对于项目的根目录（G:\\LearnRecord\\JAVA\\1_Program\\8_self-learn\\jkzzk-io）
+ *          如果使用当前项目的根目录，路径就可以简化写
+ *          G:\\LearnRecord\\JAVA\\1_Program\\8_self-learn\\jkzzk-io\\jkzzk-io.iml
+ *          可以简写为 jkzzk-io.iml
+ *      注意：
+ *          1.路径是不区分大小写的
+ *          2.windows文件分隔符是正斜杠\，必须使用转义字符\\
+ *
+ */
+public class FileDemo2 {
+
+
+}

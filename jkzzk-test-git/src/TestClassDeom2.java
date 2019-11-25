56d652b11e87385821f33219742b6e3fe7ef5a2d
jkzzk <1745493589@qq.com>
2019-11-25 10:11:48 +0800

    测试git提交

---
+++ jkzzk-test-git/src/TestClassDeom2.java
@@ -0,0 +1,11 @@
+public class TestClassDeom2 {
+
+    public static void main(String[] args) {
+        System.out.println("jkzzk");
+        System.out.println("jkzzk");
+        System.out.println("jkzzk");
+        System.out.println("jkzzk");
+        System.out.println("jkzzk");
+    }
+
+}

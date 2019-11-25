c210b0f183f1618f8ccf8f4e99df1aa059bb0993
jkzzk <1745493589@qq.com>
2019-11-25 16:43:03 +0800

    File类中判断的方法学习，第一次提交

---
+++ jkzzk-io/src/com/jkzzk/io/file/FileDemo4.java
@@ -0,0 +1,21 @@
+package com.jkzzk.io.file;
+
+/**
+ *  File判断功能的方法
+ *      1.public boolean exists(); 此File表示的文件或目录是否实际存在
+ *      2.public boolean isDirectory(); 此File表示的是否是目录
+ *      3.public boolean isFile(); 此File表示的是否是文件
+ * */
+public class FileDemo4 {
+    public static void main(String[] args) {
+        show01();
+    }
+
+    /**
+     *  public boolean exists(); 此File表示的文件或目录是否实际存在
+     *
+     * */
+    private static void show01() {
+
+    }
+}

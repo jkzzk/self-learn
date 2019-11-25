b1d1f7e294af821f297e62e50ec07284f236370d
jkzzk <1745493589@qq.com>
2019-11-25 09:42:18 +0800

    测试git提交

---
+++ jkzzk-test-git/src/TestClass.java
@@ -0,0 +1,13 @@
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("TestGit");
+        System.out.println("TestGit");
+        System.out.println("TestGit");
+        System.out.println("TestGit");
+        System.out.println("TestGit");
+        System.out.println("TestGit");
+        System.out.println("TestGit");
+    }
+
+}

0e3052c4e16775f232a6f48df4780219b77cfd6c
jkzzk <1745493589@qq.com>
2019-11-26 10:06:10 +0800

    递归联系1+....+n，第二次提交，加入使用递归的 方法

--- jkzzk-io/src/com/jkzzk/io/recursion/RecursionDemo2.java
+++ jkzzk-io/src/com/jkzzk/io/recursion/RecursionDemo2.java
@@ -6,6 +6,9 @@ package com.jkzzk.io.recursion;
  *
  *  注意：
  *      因为递归要频繁的创建方法，销毁方法，所有简单计算最好不要用递归来执行
+ *      使用递归一定要明确两点：
+ *          1.递归结束的条件
+ *          2.递归的目的
  */
 public class RecursionDemo2 {
 
